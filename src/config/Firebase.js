abd6e45fe7a65b59a345ad805daf03d5417997db
Resilda07 <85866201+Resilda07@users.noreply.github.com>
2021-09-27 21:10:31 +0200

    Initial Commit

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -3,12 +3,12 @@ import 'firebase/auth'
 import 'firebase/firestore'
 
 var firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyD0cLgN6xl4y7cmKlPmQXlznwfFTaRh7wY',
+  authDomain: 'zandaka-877bb.firebaseapp.com',
+  projectId: 'zandaka-877bb',
+  storageBucket: 'zandaka-877bb.appspot.com',
+  messagingSenderId: '1052928159244',
+  appId: '1:1052928159244:web:8ab2163b6ab130f9878829',
 }
 
 const initializeFirebase = firebase.initializeApp(firebaseConfig)
